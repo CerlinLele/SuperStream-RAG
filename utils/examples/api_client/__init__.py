d54ca2d1c7167405c5395a9121a3f5ff81ec8991
Cerlin <yilindeng1@gmail.com>
2025-12-24 01:36:19 +1100

    chore: init.py

---
+++ utils/examples/api_client/__init__.py
@@ -0,0 +1,6 @@
+"""
+API Client Configuration Examples
+
+Examples demonstrating different ways to configure and use the OpenAI API client:
+- Example 1: Custom API base configuration (for Azure, local services, etc.)
+"""
